9e429ab99af9605dca141eb2750c5f2feecbc579
Brion Mario <brion.2015283@iit.ac.lk>
2018-08-25 23:58:54 +0530

    chore(js): modify global script to match es6 syntax

--- js/global.js
+++ js/global.js
@@ -1,10 +1,10 @@
-'use strict';
-
-function getYear() {
-  var now = new Date;
-  var year = now.getYear();
+const getYear = () => {
+  let now = new Date;
+  let year = now.getYear();
   if (year < 1900) {
     year += 1900;
   }
   return year;
-}
+};
+
+module.exports = getYear;
