be71f0cd34505bee185466a100355da61ac45746
Brion Mario <brion.2015283@iit.ac.lk>
2019-01-23 05:11:04 +0530

    feat(gulp): add library support to project :sparkles:

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass');
 const header = require('gulp-header');
 const cleanCSS = require('gulp-clean-css');
-const rename = require("gulp-rename");
+const rename = require('gulp-rename');
 const sassLint = require('gulp-sass-lint');
 const eslint = require('gulp-eslint');
 const pkg = require('./package.json');
@@ -43,51 +43,20 @@ const AUTO_PREFIX_BROWSERS = [
  */
 const banner = [
   '/*! ========================================================================= *\n',
-	' *  <%= pkg.title %> \n',
-	' *  ========================================================================= *\n',
-	' *  Version: <%= pkg.version %>\n',
-	' *  Author: <%= pkg.author %>\n',
-	' *  ========================================================================= *\n',
-	' *  GitHub Repo: <%= pkg.repository.url %>\n',
-	' *  ========================================================================= *\n',
-  ' *  Copyright Ⓒ ' + (new Date()).getFullYear(),'\n',
+  ' *  <%= pkg.title %> \n',
+  ' *  ========================================================================= *\n',
+  ' *  Version: <%= pkg.version %>\n',
+  ' *  Author: <%= pkg.author %>\n',
+  ' *  ========================================================================= *\n',
+  ' *  GitHub Repo: <%= pkg.repository.url %>\n',
+  ' *  ========================================================================= *\n',
+  ' *  Copyright Ⓒ ' + new Date().getFullYear(),
+  '\n',
   ' *  Licensed under <%= pkg.license %>\n',
   ' *  ========================================================================= *\n',
   ' */\n\n',
   ''
 ].join('');
-
-const PATHS = {
-  dev: {
-    root: '.temp',
-    scripts: '.temp/js',
-    styles: '.temp/css',
-    pages: '.temp/pages',
-    components: '.temp/components',
-    assets: '.temp/assets',
-    fonts: '.temp/assets/fonts',
-  },
-  src: {
-    root: 'src',
-    scripts: 'src/scripts',
-    styles: 'src/sass',
-    pages: 'src/pages',
-    components: 'src/components',
-    index: 'src/index.html',
-    assets: 'src/assets',
-    fonts: 'src/assets/fonts',
-    vendor: 'src/vendor',
-  },
-  build: {
-    root: 'dist',
-    scripts: 'dist/js',
-    styles: 'dist/css',
-    pages: 'dist/pages',
-    assets: 'dist/assets',
-    fonts: 'dist/assets/fonts',
-    components: 'dist/components',
-  },
-};
 
 const CONFIG = {
   filenames: {
@@ -95,257 +64,380 @@ const CONFIG = {
       scripts: 'main.js',
       vendorJS: 'vendor.js',
       vendorCSS: 'vendor.css',
-      styles: 'styles.css',
+      styles: 'styles.css'
     },
-    build: {
+    prod: {
       scripts: 'main.bundle.js',
       vendorJS: 'vendor.bundle.js',
       vendorCSS: 'vendor.bundle.css',
-      styles: 'styles.bundle.css',
+      styles: 'styles.bundle.css'
+    }
+  },
+  paths: {
+    dev: {
+      root: '.temp',
+      scripts: '.temp/js',
+      styles: '.temp/css',
+      assets: '.temp/assets',
+      fonts: '.temp/assets/fonts'
+    },
+    src: {
+      root: 'src',
+      scripts: 'src/scripts',
+      styles: 'src/sass',
+      libs: 'src/libs',
+      index: 'src/index.html',
+      assets: 'src/assets',
+      fonts: 'src/assets/fonts',
+      vendor: 'src/bower_components'
+    },
+    prod: {
+      root: 'dist',
+      scripts: 'dist/js',
+      styles: 'dist/css',
+      assets: 'dist/assets',
+      fonts: 'dist/assets/fonts'
+    }
+  },
+  settings: {
+    dev: {
+      proxy: '127.0.0.1:8010',
+      tunnel: 8010,
+      port: 4200
+    },
+    prod: {
+      proxy: '127.0.0.1:8010',
+      tunnel: 8010,
+      port: 3000
     }
   }
 };
 
 gulp.task('scripts:build', () => {
-  return gulp.src([
-    `${PATHS.src.scripts}/**/*.js`,
-    `!${PATHS.src.scripts}/**/*.test.js`,
-  ])
-    .pipe(gutil.env.env === 'production'
-      ? gutil.noop()
-      : sourcemaps.init())
-    .pipe(babel({
-      presets: ['@babel/env']
-    }))
-    .pipe(gutil.env.env === 'production'
-      ? gutil.noop()
-      : sourcemaps.write())
-    .pipe(gutil.env.env === 'production'
-      ? concat(CONFIG.filenames.build.scripts)
-      : concat(CONFIG.filenames.dev.scripts))
-    .pipe(gutil.env.env === 'production'
-      ? uglify()
-      : gutil.noop())
-    .pipe(gutil.env.env === 'production'
-      ? rename({
-        suffix: '.min'
+  return gulp
+    .src([`${CONFIG.paths.src.scripts}/**/*.js`, `!${CONFIG.paths.src.scripts}/**/*.test.js`])
+    .pipe(gutil.env.env === 'production' ? gutil.noop() : sourcemaps.init())
+    .pipe(
+      babel({
+        presets: ['@babel/env']
       })
-      : gutil.noop())
-    .pipe(gutil.env.env === 'production'
-      ? header(banner, {
-        pkg: pkg
-      })
-      : gutil.noop())
-    .pipe(gutil.env.env === 'production'
-      ? gulp.dest(`${PATHS.build.scripts}`)
-      : gulp.dest(`${PATHS.dev.scripts}`))
+    )
+    .pipe(gutil.env.env === 'production' ? gutil.noop() : sourcemaps.write())
+    .pipe(
+      gutil.env.env === 'production'
+        ? concat(CONFIG.filenames.prod.scripts)
+        : concat(CONFIG.filenames.dev.scripts)
+    )
+    .pipe(gutil.env.env === 'production' ? uglify() : gutil.noop())
+    .pipe(
+      gutil.env.env === 'production'
+        ? rename({
+            suffix: '.min'
+          })
+        : gutil.noop()
+    )
+    .pipe(
+      gutil.env.env === 'production'
+        ? header(banner, {
+            pkg: pkg
+          })
+        : gutil.noop()
+    )
+    .pipe(
+      gutil.env.env === 'production'
+        ? gulp.dest(`${CONFIG.paths.prod.scripts}`)
+        : gulp.dest(`${CONFIG.paths.dev.scripts}`)
+    );
 });
 
 gulp.task('scripts:lint', () => {
-  return gulp.src(`${PATHS.src.scripts}/**/*.js`)
+  return gulp
+    .src(`${CONFIG.paths.src.scripts}/**/*.js`)
     .pipe(eslint())
-    .pipe(eslint.format())
+    .pipe(eslint.format());
 });
 
 gulp.task('styles:build', () => {
-  return gulp.src(`${PATHS.src.styles}/**/*.s+(a|c)ss`)
-    .pipe(sass({
-      outputStyle: 'nested',
-      precision: 10,
-      includePaths: ['.'],
-      onError: console.error.bind(console, 'Sass error:')
-    }))
-    .pipe(autoprefixer({browsers: AUTO_PREFIX_BROWSERS}))
-    .pipe(urlAdjuster({
-      prependRelative: '../',
-    }))
-    .pipe(gutil.env.env === 'production'
-      ? concat(CONFIG.filenames.build.styles)
-      : concat(CONFIG.filenames.dev.styles))
-    .pipe(gutil.env.env === 'production'
-      ? header(banner, {
-        pkg: pkg
+  return gulp
+    .src(`${CONFIG.paths.src.styles}/**/*.s+(a|c)ss`)
+    .pipe(
+      sass({
+        outputStyle: 'nested',
+        precision: 10,
+        includePaths: ['.'],
+        onError: console.error.bind(console, 'Sass error:')
       })
-      : gutil.noop())
-    .pipe(gutil.env.env === 'production'
-      ? cleanCSS()
-      : gutil.noop())
-    .pipe(gutil.env.env === 'production'
-      ? rename({
-        suffix: '.min'
+    )
+    .pipe(autoprefixer({ browsers: AUTO_PREFIX_BROWSERS }))
+    .pipe(
+      urlAdjuster({
+        prependRelative: '../'
       })
-      : gutil.noop())
-    .pipe(gutil.env.env === 'production'
-      ? gulp.dest(PATHS.build.styles)
-      : gulp.dest(PATHS.dev.styles))
+    )
+    .pipe(
+      gutil.env.env === 'production'
+        ? concat(CONFIG.filenames.prod.styles)
+        : concat(CONFIG.filenames.dev.styles)
+    )
+    .pipe(
+      gutil.env.env === 'production'
+        ? header(banner, {
+            pkg: pkg
+          })
+        : gutil.noop()
+    )
+    .pipe(gutil.env.env === 'production' ? cleanCSS() : gutil.noop())
+    .pipe(
+      gutil.env.env === 'production'
+        ? rename({
+            suffix: '.min'
+          })
+        : gutil.noop()
+    )
+    .pipe(
+      gutil.env.env === 'production'
+        ? gulp.dest(CONFIG.paths.prod.styles)
+        : gulp.dest(CONFIG.paths.dev.styles)
+    )
     .pipe(browserSync.stream());
 });
 
-gulp.task('styles:lint', function () {
-  return gulp.src(`${PATHS.src.root}/**/*.s+(a|c)ss`)
-    .pipe(sassLint({
-      options: {
-        formatter: 'stylish',
-        'merge-default-rules': false
-      },
-      files: {ignore: 'node_modules/!**!/!*.s+(a|c)ss'},
-      rules: {
-        'no-ids': 1,
-        'no-mergeable-selectors': 0
-      },
-      configFile: '.sass-lint.yml'
-    }))
-    .pipe(sassLint.format())
+gulp.task('styles:lint', () => {
+  return gulp
+    .src(`${CONFIG.paths.src.root}/**/*.s+(a|c)ss`)
+    .pipe(
+      sassLint({
+        options: {
+          formatter: 'stylish',
+          'merge-default-rules': false
+        },
+        files: { ignore: 'node_modules/!**!/!*.s+(a|c)ss' },
+        rules: {
+          'no-ids': 1,
+          'no-mergeable-selectors': 0
+        },
+        configFile: '.sass-lint.yml'
+      })
+    )
+    .pipe(sassLint.format());
 });
 
 gulp.task('clean', () => {
   if (gutil.env.env === 'production') {
-    return del(PATHS.build.root);
-  } else if (gutil.env.env === 'development') {
-    return del(PATHS.dev.root);
-  } else if (gutil.env.env === 'all') {
-    return del([PATHS.dev.root, PATHS.build.root])
-  } else {
-    return del([PATHS.dev.root, PATHS.build.root])
+    return del(CONFIG.paths.prod.root);
   }
+  if (gutil.env.env === 'development') {
+    return del(CONFIG.paths.dev.root);
+  }
+  if (gutil.env.env === 'all') {
+    return del([CONFIG.paths.dev.root, CONFIG.paths.prod.root]);
+  }
+  return del([CONFIG.paths.dev.root, CONFIG.paths.prod.root]);
 });
 
 gulp.task('move:assets', () => {
-  return gulp.src([
-    `${PATHS.src.assets}/**/*`,
-  ], { base: PATHS.src.root })
-    .pipe(gutil.env.env === 'production'
-      ? imagemin()
-      : gutil.noop())
-    .pipe(gutil.env.env === 'production'
-      ? gulp.dest(PATHS.build.root)
-      : gulp.dest(PATHS.dev.root))
+  return gulp
+    .src([`${CONFIG.paths.src.assets}/**/*`], { base: CONFIG.paths.src.root })
+    .pipe(gutil.env.env === 'production' ? imagemin() : gutil.noop())
+    .pipe(
+      gutil.env.env === 'production'
+        ? gulp.dest(CONFIG.paths.prod.root)
+        : gulp.dest(CONFIG.paths.dev.root)
+    );
+});
+
+gulp.task('move:libs', () => {
+  return gulp
+    .src([`${CONFIG.paths.src.libs}/**/*`], { base: CONFIG.paths.src.root })
+    .pipe(
+      gutil.env.env === 'production'
+        ? gulp.dest(CONFIG.paths.prod.root)
+        : gulp.dest(CONFIG.paths.dev.root)
+    );
 });
 
 gulp.task('move:vendor:fonts', () => {
-  return gulp.src(bowerlibs('**/*.{eot,svg,ttf,woff,woff2}'))
-    .pipe(gutil.env.env === 'production'
-      ? gulp.dest(PATHS.build.fonts)
-      : gulp.dest(PATHS.dev.fonts)
-  );
+  return gulp
+    .src(bowerlibs('**/*.{eot,svg,ttf,woff,woff2}'))
+    .pipe(
+      gutil.env.env === 'production'
+        ? gulp.dest(CONFIG.paths.prod.fonts)
+        : gulp.dest(CONFIG.paths.dev.fonts)
+    );
 });
 
 gulp.task('bundle:vendor', () => {
-  let target = gulp.src([
-    PATHS.src.index,
-    `${PATHS.src.components}/**/*.{html|jade|php}`,
-    `${PATHS.src.pages}/**/*.{html|jade|php}`,
-  ], { base: PATHS.src.root });
+  let target = gulp.src(
+    [
+      CONFIG.paths.src.index,
+      `${CONFIG.paths.src.root}/**/*.html`,
+      `${CONFIG.paths.src.root}/**/*.jade`,
+      `${CONFIG.paths.src.root}/**/*.php`,
+      `!${CONFIG.paths.src.vendor}/**/*`,
+      `!${CONFIG.paths.src.libs}/**/*`
+    ],
+    { base: CONFIG.paths.src.root }
+  );
 
   let js = gulp.src(wiredep().js);
   let css = gulp.src(wiredep().css);
 
   return target
     .pipe(
-      inject(js.pipe(concat(gutil.env.env === 'production'
-        ? CONFIG.filenames.build.vendorJS
-        : CONFIG.filenames.dev.vendorJS))
-        .pipe(gutil.env.env === 'production'
-          ? gulp.dest(PATHS.build.scripts)
-          : gulp.dest(PATHS.dev.scripts))
+      inject(
+        js
+          .pipe(
+            concat(
+              gutil.env.env === 'production'
+                ? CONFIG.filenames.prod.vendorJS
+                : CONFIG.filenames.dev.vendorJS
+            )
+          )
+          .pipe(
+            gutil.env.env === 'production'
+              ? gulp.dest(CONFIG.paths.prod.scripts)
+              : gulp.dest(CONFIG.paths.dev.scripts)
+          )
       )
     )
     .pipe(
-      inject(css.pipe(concat(gutil.env.env === 'production'
-        ? CONFIG.filenames.build.vendorCSS
-        : CONFIG.filenames.dev.vendorCSS))
-        .pipe(urlAdjuster({
-          replace:  ['../fonts','../assets/fonts'],
-        }))
-        .pipe(gutil.env.env === 'production'
-          ? gulp.dest(PATHS.build.styles)
-          : gulp.dest(PATHS.dev.styles))
+      inject(
+        css
+          .pipe(
+            concat(
+              gutil.env.env === 'production'
+                ? CONFIG.filenames.prod.vendorCSS
+                : CONFIG.filenames.dev.vendorCSS
+            )
+          )
+          .pipe(
+            urlAdjuster({
+              replace: ['../fonts', '../assets/fonts']
+            })
+          )
+          .pipe(
+            gutil.env.env === 'production'
+              ? gulp.dest(CONFIG.paths.prod.styles)
+              : gulp.dest(CONFIG.paths.dev.styles)
+          )
       )
-    )
+    );
 });
 
 gulp.task('inject', () => {
-  let target = gulp.src([
-    PATHS.src.index,
-    `${PATHS.src.components}/**/*.{html|jade|php}`,
-    `${PATHS.src.pages}/**/*.{html|jade|php}`,
-  ], { base: PATHS.src.root });
+  let target = gulp.src(
+    [
+      `${CONFIG.paths.src.root}/**/*.html`,
+      `${CONFIG.paths.src.root}/**/*.jade`,
+      `${CONFIG.paths.src.root}/**/*.php`,
+      `!${CONFIG.paths.src.vendor}/**/*`,
+      `!${CONFIG.paths.src.libs}/**/*`
+    ],
+    { base: CONFIG.paths.src.root }
+  );
 
   let devSources = gulp.src(
-    [`${PATHS.dev.scripts}/**/*.js`, `${PATHS.dev.styles}/**/*.css`, `!${PATHS.dev.scripts}/**/*.test.js`,],
+    [
+      `${CONFIG.paths.dev.scripts}/**/*.js`,
+      `${CONFIG.paths.dev.styles}/**/*.css`,
+      `!${CONFIG.paths.dev.scripts}/**/*.test.js`
+    ],
     {
       read: false
     }
   );
 
   let devInjectionOptions = {
-    ignorePath: PATHS.dev.root,
+    ignorePath: CONFIG.paths.dev.root,
     addRootSlash: false
   };
 
   let prodSources = gulp.src(
-    [`${PATHS.build.scripts}/**/*.js`, `${PATHS.build.styles}/**/*.css`, `!${PATHS.build.scripts}/**/*.test.js`,],
-    {read: false}
+    [
+      `${CONFIG.paths.prod.scripts}/**/*.js`,
+      `${CONFIG.paths.prod.styles}/**/*.css`,
+      `!${CONFIG.paths.prod.scripts}/**/*.test.js`
+    ],
+    { read: false }
   );
 
   let prodInjectionOptions = {
-    ignorePath: PATHS.build.root,
+    ignorePath: CONFIG.paths.prod.root,
     addRootSlash: false
   };
 
   return target
-    .pipe(gutil.env.env === 'production'
-      ? htmlmin({
-        collapseWhitespace: true,
-      })
-      : gutil.noop())
+    .pipe(
+      gutil.env.env === 'production'
+        ? htmlmin({
+            collapseWhitespace: true
+          })
+        : gutil.noop()
+    )
     .pipe(
       gutil.env.env === 'production'
         ? inject(prodSources, prodInjectionOptions)
         : inject(devSources, devInjectionOptions)
     )
-    .pipe(gutil.env.env === 'production'
-      ? gulp.dest(PATHS.build.root)
-      : gulp.dest(PATHS.dev.root))
+    .pipe(
+      gutil.env.env === 'production'
+        ? gulp.dest(CONFIG.paths.prod.root)
+        : gulp.dest(CONFIG.paths.dev.root)
+    );
 });
 
-gulp.task('build',
+gulp.task(
+  'build',
   gulp.series(
-    'move:assets', 'move:vendor:fonts', gulp.parallel('scripts:build', 'styles:build'), 'bundle:vendor', 'inject'
-  ), (callback) => {
+    'move:assets',
+    'move:vendor:fonts',
+    gulp.parallel('scripts:build', 'styles:build'),
+    'bundle:vendor',
+    'inject'
+  ),
+  callback => {
     callback();
-});
+  }
+);
 
-gulp.task('browserSync', (callback) => {
+gulp.task('browserSync', callback => {
   browserSync.init({
     server: {
-      baseDir: PATHS.dev.root
-    },
+      baseDir: CONFIG.paths.dev.root
+    }
   });
   callback();
 });
 
 gulp.task('watch:styles', () => {
-  gulp.watch(`${PATHS.src.styles}/**/*.s+(a|c)ss`, gulp.series('styles:build'))
+  gulp.watch(`${CONFIG.paths.src.styles}/**/*.s+(a|c)ss`, gulp.series('styles:build'));
 });
 
 gulp.task('watch:scripts', () => {
-  gulp.watch(`${PATHS.src.scripts}/**/*.js`, gulp.series('scripts:build'))
+  gulp
+    .watch(`${CONFIG.paths.src.scripts}/**/*.js`, gulp.series('scripts:build'))
     .on('change', browserSync.reload);
 });
 
 gulp.task('watch:pages', () => {
-  gulp.watch([
-    PATHS.src.index, `${PATHS.src.components}/**/*`, `${PATHS.src.pages}/**/*`
-  ], gulp.series('inject'))
+  gulp
+    .watch(
+      [
+        `${CONFIG.paths.src.root}/**/*.php`,
+        `${CONFIG.paths.src.root}/**/*.html`,
+        `${CONFIG.paths.src.root}/**/*.jade`
+      ],
+      gulp.series('inject')
+    )
     .on('change', browserSync.reload);
 });
 
 gulp.task('watch:assets', () => {
-  gulp.watch(`${PATHS.src.assets}/**/*`, gulp.series('move:assets'))
+  gulp
+    .watch(`${CONFIG.paths.src.assets}/**/*`, gulp.series('move:assets', 'move:libs'))
     .on('change', browserSync.reload);
 });
 
 gulp.task('watch', gulp.parallel('watch:styles', 'watch:scripts', 'watch:pages', 'watch:assets'));
 
-gulp.task('serve:dev', gulp.series('browserSync', 'watch'));
+gulp.task('serve', gulp.series('browserSync', 'watch'));
